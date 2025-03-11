d85486d74cd9472d2f3bd5682c62a81d03fbf222
Your Name <you@example.com>
2025-03-11 20:26:43 +0200

    Create backend Flask app. GET / and /api/chat/<room> implemented.

---
+++ backend.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='.')
+
+# Dictionary to store chat
+chats = {}
+
+
+@app.route('/', methods=['GET'])
+def serve_html():
+    # Serve the HTML file
+    return render_template('index.html')
+
+
+@app.route('/api/chat/<room>', methods=['GET'])
+def get_chat(room):
+    # Return chat for the room or an empty string if no messages exist
+    return "\n".join(chats.get(room, []))
+
+
+if __name__ == '__main__':
+    # Run the Flask app
+    app.run(debug=True)
+
