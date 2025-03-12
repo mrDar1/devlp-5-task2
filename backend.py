01166735ba1c4fd09e14dcf00261dba8886cb2ad
Dor Solomon <68469554+dor-solomon@users.noreply.github.com>
2025-03-12 12:46:21 +0200

    Change temp dictionary storage into file-based. Add chats.json to .gitignore

--- backend.py
+++ backend.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__, template_folder='.')
 
-# Dictionary to store chat
-chats = {}
+# JSON file to store chat
+chats_file = 'chats.json'
+
+# Load JSON file or initialize empty JSON
+def load_chats():
+    try:
+        with open(chats_file, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+def save_chats(chats):
+    with open(chats_file, 'w') as file:
+        json.dump(chats, file)
+
+# Initialize chats variable from file
+chats = load_chats()
 
 # Implemented by Dor
 @app.route('/', methods=['GET'])
@@ -21,4 +37,3 @@ def get_chat(room):
 if __name__ == '__main__':
     # Run the Flask app
     app.run(debug=True)
-
